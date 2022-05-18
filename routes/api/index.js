ad11fe037d2b078d599f36805e2e57ded3355a09
Adam McLane <mclanea92@gmail.com>
2022-05-18 14:00:08 -0400

    started repo over, node modules were giving me issues

---
+++ routes/api/index.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const data = require('./data/db.json');
+const { addNote, deleteNote } = require('../..lib/data.js');
+
+router.get('/notes', (req, res) => {
+    res.send(data);
+});
+
+router.get('/', (req, res) => {
+    res.send('API HERE!');
+});
+
+router.post('/notes', (req, res) => {
+    addNote(req.body);
+    res.json();
+});
+
+router.delete('/notes', (req, res) => {
+    deleteNote(req.params);
+    res.json();
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+
+// added express from npm
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+// port for server and for 3001 localhost server
+const PORT = process.env.PORT || 3001;
+
+const apiRoute = require('./routes/api');
+const htmlRoute = require('./routes/html');
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/api', apiRoute);
+
+app.use('/', htmlRoute);
+
+app.listen(PORT, () => {
+    console.log(`${PORT} is the current server`)
+});
