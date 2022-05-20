7e80ebcb31f5146abf7edb798af5c48658c8d8e4
Adam McLane <mclanea92@gmail.com>
2022-05-20 13:58:23 -0400

    added notes

--- helpers/uuid.js
+++ helpers/uuid.js
@@ -1,4 +1,4 @@
-// Immediately export a function that generates a string of random numbers and letters
+//added this function to give a unique id to each note in the json file containing all notes.
 module.exports = () =>
   Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
